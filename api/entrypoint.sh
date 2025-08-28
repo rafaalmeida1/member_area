073c9add853592747abc805568891ac67ddbff75
Rafael Almeida <rafaprof312@gmail.com>
2025-08-27 21:23:57 -0300

    Adiciona script de deploy para a Nutri Thata API, que verifica variáveis de ambiente, aguarda a disponibilidade do banco de dados, executa reparos e migrações do Flyway, e inicia a aplicação Spring Boot.

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -7,14 +7,6 @@ while ! nc -z $DB_HOST $DB_PORT; do
 done
 echo "Banco de dados está disponível!"
 
-# Executar repair do Flyway se necessário
-echo "Executando repair do Flyway..."
-java -cp app.jar org.flywaydb.commandline.Main repair -url=jdbc:postgresql://$DB_HOST:$DB_PORT/$DB_NAME -user=$DB_USERNAME -password=$DB_PASSWORD
-
-# Executar migrations do Flyway
-echo "Executando migrations do Flyway..."
-java -cp app.jar org.flywaydb.commandline.Main migrate -url=jdbc:postgresql://$DB_HOST:$DB_PORT/$DB_NAME -user=$DB_USERNAME -password=$DB_PASSWORD
-
-# Iniciar a aplicação
+# Iniciar a aplicação (o Spring Boot executará as migrations automaticamente)
 echo "Iniciando aplicação..."
 exec java -jar app.jar 
